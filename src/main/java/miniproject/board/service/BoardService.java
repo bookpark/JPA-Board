b8ccdace305281fe4c739f7b4006a2a5036c23a8
bookpark <bkbkgg@gmail.com>
2022-11-03 13:50:27 +0900

    Service에 save, show, delete 메서드 추가

---
+++ src/main/java/miniproject/board/service/BoardService.java
@@ -0,0 +1,43 @@
+package miniproject.board.service;
+
+import miniproject.board.domain.Board;
+import miniproject.board.repository.SpringDataJpaBoardRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BoardService {
+
+    private final SpringDataJpaBoardRepository boardRepository;
+
+    public BoardService(SpringDataJpaBoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Long saveBoard(Board board) {
+        boardRepository.save(board);
+        return board.getId();
+    }
+
+//    public Optional<Board> showBoardById(Long id) {
+//        return boardRepository.findById(id);
+//    }
+
+    public Board showBoardById(Long id) {
+        return boardRepository.findById(id).get();
+    }
+
+    public List<Board> showBoardList(Board board) {
+        return boardRepository.findAll();
+    }
+
+    public void deleteBoardById(Long id) {
+        boardRepository.deleteById(id);
+    }
+
+
+}
