7059fb4643bf55cc98caff5d3a02ae44bccd4178
bookpark <bkbkgg@gmail.com>
2022-11-03 13:59:42 +0900

    게시글 작성 controller 추가

--- src/main/java/miniproject/board/controller/BoardController.java
+++ src/main/java/miniproject/board/controller/BoardController.java
@@ -1,12 +1,21 @@
 package miniproject.board.controller;
 
 import miniproject.board.domain.Board;
+import miniproject.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+
+    private BoardService service;
+
+    @Autowired
+    public BoardController(BoardService service) {
+        this.service = service;
+    }
 
     @GetMapping("/board-list")
     public String home() {
@@ -25,7 +34,7 @@ public class BoardController {
 
     @PostMapping("/board-write")
     public String boardWritePost(Board board, String title, String content) {
-        System.out.println(board.getTitle() + " " + board.getContent());
+        service.saveBoard(board);
         return ("redirect:/board-list");
     }
 
