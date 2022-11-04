aea9b3288a7edd7c22565ac79eff392ea47741d2
bookpark <bkbkgg@gmail.com>
2022-11-04 14:34:47 +0900

    lombok 활용 getter/setter 추가

--- src/main/java/miniproject/board/domain/Member.java
+++ src/main/java/miniproject/board/domain/Member.java
@@ -1,11 +1,16 @@
 package miniproject.board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id
@@ -17,43 +22,4 @@ public class Member {
     private String email;
     private String address;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
